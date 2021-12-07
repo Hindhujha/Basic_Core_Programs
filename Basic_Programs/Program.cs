a3083add6f77cdc0c94d97eca9e9abab4f398051
unknown <hindhujhasridhar@gmail.com>
2021-12-07 21:48:07 +0530

    BASIC CORE PROGRAMS

---
+++ Basic_Programs/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+namespace Basic_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BASIC_CORE_PROGRAMS");
+        }
+    }
+}

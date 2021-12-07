4258bf2a33e01ad2a725876e6af9a51d06f2b65f
unknown <hindhujhasridhar@gmail.com>
2021-12-07 22:13:40 +0530

    FIND EVEN/ODD

---
+++ Basic_Programs/EvenOdd.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core.EvenorOdd
+{
+    public  class EvenOdd
+
+    {
+        public void EVENODD()
+        {
+            int n;
+            Console.WriteLine("ENTER A NUMBER");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n % 2 == 0)
+            {
+                Console.WriteLine(n + " IS A EVEN NUMBER");
+            }
+            else
+            {
+                Console.WriteLine(n + " IS A ODD NUMBER");
+            }
+        }
+    }
+}
--- Basic_Programs/Program.cs
+++ Basic_Programs/Program.cs
@@ -8,6 +8,22 @@ namespace Basic_Core
         static void Main(string[] args)
         {
             Console.WriteLine("BASIC_CORE_PROGRAMS");
+            Console.WriteLine("1.EVEN/ODD");
+            int select=Convert.ToInt32(Console.ReadLine());
+          
+            while (true)
+            {
+                switch(select)
+                {
+                    case 1:
+                        EvenorOdd.EvenOdd evenOdd = new  EvenorOdd.EvenOdd();
+                        evenOdd.EVENODD();
+                        break;
+                    default:
+                        break;
+
+                }
+            }
         }
     }
 }

987aee326ceb4c9b81814f0631d9eab6f7262478
unknown <hindhujhasridhar@gmail.com>
2021-12-07 22:27:12 +0530

    FIND VOWEL/CONSONANT

---
+++ Basic_Programs/Alphabet.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core.Alphabet
+{
+    internal class Alphabet
+    {
+        public void Alph()
+        {
+            Console.WriteLine("ENTER AN ALPHABET: ");
+
+            char alphabet = Console.ReadKey(true).KeyChar;
+            switch (alphabet)
+            {
+                case 'a':
+                case 'A':
+                case 'e':
+                case 'E':
+                case 'i':
+                case 'I':
+                case 'o':
+                case 'O':
+                case 'u':
+                case 'U':
+                    Console.WriteLine( "VOWEL");
+                    break;
+                default:
+                    Console.WriteLine(alphabet + "CONSONANT");
+                    break;
+            }
+        }
+    }
+}
+
--- Basic_Programs/Program.cs
+++ Basic_Programs/Program.cs
@@ -9,6 +9,7 @@ namespace Basic_Core
         {
             Console.WriteLine("BASIC_CORE_PROGRAMS");
             Console.WriteLine("1.EVEN/ODD");
+            Console.WriteLine("2.VOWEL/CONSONANT");
             int select=Convert.ToInt32(Console.ReadLine());
           
             while (true)
@@ -18,6 +19,10 @@ namespace Basic_Core
                     case 1:
                         EvenorOdd.EvenOdd evenOdd = new  EvenorOdd.EvenOdd();
                         evenOdd.EVENODD();
+                        break;
+                        case 2:
+                            Alphabet.Alphabet alpha=new Alphabet.Alphabet();
+                        alpha.Alph();
                         break;
                     default:
                         break;

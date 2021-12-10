c9f116a0cead57d871ec17b3126c4892e79080d8
unknown <hindhujhasridhar@gmail.com>
2021-12-10 20:24:03 +0530

    SWAPPING OF 2 NUMBERS

--- Basic_Programs/Program.cs
+++ Basic_Programs/Program.cs
@@ -10,25 +10,31 @@ namespace Basic_Core
             Console.WriteLine("BASIC_CORE_PROGRAMS");
             Console.WriteLine("1.EVEN/ODD");
             Console.WriteLine("2.VOWEL/CONSONANT");
+            Console.WriteLine("3.SWAPPING OF 2 NUMBERS");
             int select=Convert.ToInt32(Console.ReadLine());
           
-            while (true)
-            {
+           
                 switch(select)
                 {
                     case 1:
-                        EvenorOdd.EvenOdd evenOdd = new  EvenorOdd.EvenOdd();
+                        EvenorOdd.EvenOdd evenOdd = new EvenorOdd.EvenOdd();
                         evenOdd.EVENODD();
                         break;
                         case 2:
                             Alphabet.Alphabet alpha=new Alphabet.Alphabet();
                         alpha.Alph();
                         break;
+                    case 3:
+                      Swap.Swap myobj= new Swap.Swap();
+                        myobj.Swap2_numbers();
+                        break;
                     default:
+                        Console.WriteLine("ENTER ANOTHER PROBLEM NO:");
                         break;
 
                 }
+                
             }
+           
         }
     }
-}
---
+++ Basic_Programs/Swap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core.Swap
+{
+   public class Swap
+    {
+        public void Swap2_numbers()
+        {
+            int num1, num2, temp;
+            Console.WriteLine("BEFORE SWAPPING: ");
+            Console.WriteLine("ENTER TWO NUMBERS");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+            Console.WriteLine("AFTER SWAPPING: ");
+            Console.WriteLine("FIRST NUMBER: " + num1);
+            Console.WriteLine("SECOND NUMBER: " + num2);
+
+        }
+
+    }
+}

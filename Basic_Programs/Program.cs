aa2d5dd19c2a41512a395e8ba36afb1f38e5a12b
unknown <hindhujhasridhar@gmail.com>
2021-12-10 20:45:17 +0530

    LARGEST OF 3 NUMBERS

--- Basic_Programs/Program.cs
+++ Basic_Programs/Program.cs
@@ -11,6 +11,7 @@ namespace Basic_Core
             Console.WriteLine("1.EVEN/ODD");
             Console.WriteLine("2.VOWEL/CONSONANT");
             Console.WriteLine("3.SWAPPING OF 2 NUMBERS");
+            Console.WriteLine("4.LARGEST OF 3 NUMBERS");
             int select=Convert.ToInt32(Console.ReadLine());
           
            
@@ -28,6 +29,10 @@ namespace Basic_Core
                       Swap.Swap myobj= new Swap.Swap();
                         myobj.Swap2_numbers();
                         break;
+                case 4:
+                    Largest.Large_Number myObj1 = new Largest.Large_Number();
+                    myObj1.large();
+                    break;
                     default:
                         Console.WriteLine("ENTER ANOTHER PROBLEM NO:");
                         break;

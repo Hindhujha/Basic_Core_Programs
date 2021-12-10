a8a036c4110b19e33e41df5da4fbb8c62e86a0e1
unknown <hindhujhasridhar@gmail.com>
2021-12-10 22:25:35 +0530

    FIND LEAP YEAR OR NOT

---
+++ Basic_Programs/LeapYear.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Core.LeapYear
+{
+    public class LeapYear
+    {
+        public void years()
+        {
+            Console.WriteLine("ENTER A YEAR");
+            int year=Convert.ToInt32(Console.ReadLine());
+            if( (year%400==0) || (year%100!=0 && year%4==0) )
+            {
+                Console.WriteLine("IT IS A LEAP YEAR");
+            }
+            else
+            {
+                Console.WriteLine("IT IS NOT A LEAP YEAR");
+            }
+        
+        }
+    }
+}
--- Basic_Programs/Program.cs
+++ Basic_Programs/Program.cs
@@ -6,45 +6,53 @@ namespace Basic_Core
     class Program
     {
         static void Main(string[] args)
-        {
+        {     
             Console.WriteLine("BASIC_CORE_PROGRAMS");
-            Console.WriteLine("1.EVEN/ODD");
-            Console.WriteLine("2.VOWEL/CONSONANT");
-            Console.WriteLine("3.SWAPPING OF 2 NUMBERS");
-            Console.WriteLine("4.LARGEST OF 3 NUMBERS");
-            Console.WriteLine("5.QUOTIENT & REMAINDER");
-            int select=Convert.ToInt32(Console.ReadLine());
-          
-           
-                switch(select)
+            while (true)
+            {
+                Console.WriteLine("-----------------");
+                Console.WriteLine("1.EVEN/ODD");
+                Console.WriteLine("2.VOWEL/CONSONANT");
+                Console.WriteLine("3.SWAPPING OF 2 NUMBERS");
+                Console.WriteLine("4.LARGEST OF 3 NUMBERS");
+                Console.WriteLine("5.QUOTIENT & REMAINDER");
+                Console.WriteLine("6.LEAP YEAR OR NOT");
+                int select = Convert.ToInt32(Console.ReadLine());
+
+                switch (select)
                 {
                     case 1:
                         EvenorOdd.EvenOdd evenOdd = new EvenorOdd.EvenOdd();
                         evenOdd.EVENODD();
                         break;
-                        case 2:
-                            Alphabet.Alphabet alpha=new Alphabet.Alphabet();
+                    case 2:
+                        Alphabet.Alphabet alpha = new Alphabet.Alphabet();
                         alpha.Alph();
                         break;
                     case 3:
-                      Swap.Swap myobj= new Swap.Swap();
+                        Swap.Swap myobj = new Swap.Swap();
                         myobj.Swap2_numbers();
                         break;
-                case 4:
-                    Largest.Large_Number myObj1 = new Largest.Large_Number();
-                    myObj1.large();
-                    break;
-                case 5:
-                    Compute.Quotient_Remainder div = new Compute.Quotient_Remainder();
-                    div.program();
-                    break;
-                    default:
-                        Console.WriteLine("ENTER ANOTHER PROBLEM NO:");
+                    case 4:
+                        Largest.Large_Number myObj1 = new Largest.Large_Number();
+                        myObj1.large();
                         break;
-
+                    case 5:
+                        Compute.Quotient_Remainder div = new Compute.Quotient_Remainder();
+                        div.program();
+                        break;
+                    case 6:
+                        LeapYear.LeapYear year = new LeapYear.LeapYear();
+                        year.years();
+                        break;
+                    default:
+                        Console.WriteLine("ENTER A CORRECT NO:");
+                        break;
                 }
-                
+            }
+               
+            }
             }
            
         }
-    }
+    
